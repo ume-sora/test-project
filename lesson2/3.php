171a8c399ddbd2782adf8e45ad97603b7e8148f1
umetsu <m.umetsu@so-ra.co.jp>
2025-04-11 15:20:29 +0900

    L2の問題

--- lesson2/3.php
+++ lesson2/3.php
@@ -6,4 +6,20 @@
 //70点以上 -> C
 //60点以上 -> D
 //60点未満 -> E
+
+$score = 50;
+if ($score == 100){ //100と等しかったらAA
+    echo "AA";
+} else if ($score >= 90){ //90以上ならA
+    echo "A";
+} else if ($score >= 80){ //80以上ならB
+    echo "B";
+} else if ($score >= 70){ //70以上ならC
+    echo "C";
+} else if ($score >= 60){ //60以上ならD
+    echo "D";
+} else {
+    echo "E"; //それ以下ならE
+}
+echo "\n";
 ?>
